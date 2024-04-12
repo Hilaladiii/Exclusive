2aee5419d1365aeb4e05dcd96261054cdd48f0f1
Hilaladiii <abdulhilal242@gmail.com>
2024-04-12 19:54:42 +0700

    style : change padding button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,8 +20,8 @@ const Button = ({
       className={clsx(
         "text-base font-medium rounded transition-colors duration-300",
         {
-          "text-white py-4 px-12": size === "big",
-          "text-white py-3 px-8": size === "small",
+          "text-white py-3 px-12": size === "big",
+          "text-white py-2 px-8": size === "small",
           "bg-secondary2 hover:bg-hover1": variant === "primary",
           "text-blacks hover:text-text1 bg-white hover:bg-white/30 border border-black/50 hover:border-black/30":
             variant === "secondary",
