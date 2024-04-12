f21f9b74de4f300a89ae97e7b1efc2a568298d31
Hilaladiii <abdulhilal242@gmail.com>
2024-04-12 19:55:41 +0700

    refactor : delete unused code

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -1,6 +1,5 @@
 import Button from "@/components/ui/button";
 import Input from "@/components/ui/input";
-import { UserType, userSchema } from "@/types/user";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
 import { useForm } from "react-hook-form";
