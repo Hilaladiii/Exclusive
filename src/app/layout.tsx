afbc33e154a9c63b3604d8e915fa15f01e6cc2ab
Hilaladiii <abdulhilal242@gmail.com>
2024-04-12 17:27:33 +0700

    refactor : change font using poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
+import { AppShell } from "@/components/layouts/AppShell";
 
-const inter = Inter({ subsets: ["latin"] });
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +20,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={poppins.className}>
+        <AppShell>{children}</AppShell>
+      </body>
     </html>
   );
 }
