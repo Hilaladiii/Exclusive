d5d7932b3fe9d0777e961e83e146074c109b904b
Hilaladiii <abdulhilal242@gmail.com>
2024-04-13 08:57:53 +0700

    feat : add next auth provider in app shell

--- src/components/layouts/AppShell.tsx
+++ src/components/layouts/AppShell.tsx
@@ -1,16 +1,19 @@
 "use client";
 import Footer from "../Footer";
 import Navbar from "../Navbar";
-import { ReactQueryProvider } from "@/providers/tanstackProviders";
+import { ReactQueryProvider } from "@/providers/TanstackProviders";
+import { NextAuthProvider } from "@/providers/NextAuthProvider";
 
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <main>
-      <ReactQueryProvider>
-        <Navbar />
-        {children}
-        <Footer />
-      </ReactQueryProvider>
+      <NextAuthProvider>
+        <ReactQueryProvider>
+          <Navbar />
+          {children}
+          <Footer />
+        </ReactQueryProvider>
+      </NextAuthProvider>
     </main>
   );
 }
