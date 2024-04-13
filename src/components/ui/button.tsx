284093dd09bb5ca3c179bf94d5acbd06ce80ad41
Hilaladiii <abdulhilal242@gmail.com>
2024-04-13 07:46:03 +0700

    fix : resolve error conflict style

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -20,10 +20,10 @@ const Button = ({
       className={clsx(
         "text-base font-medium rounded transition-colors duration-300",
         {
-          "text-white py-3 px-12": size === "big",
-          "text-white py-2 px-8": size === "small",
-          "bg-secondary2 hover:bg-hover1": variant === "primary",
-          "text-blacks hover:text-text1 bg-white hover:bg-white/30 border border-black/50 hover:border-black/30":
+          "py-3 px-12": size === "big",
+          "py-2 px-8": size === "small",
+          "text-white bg-secondary2 hover:bg-hover1": variant === "primary",
+          "text-black hover:text-text1 bg-white hover:bg-white/30 border border-black/50 hover:border-black/30":
             variant === "secondary",
         },
         className
