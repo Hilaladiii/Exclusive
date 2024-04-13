b3d3080d0b93a97b788d320c0300f9c61b0dbf1d
Hilaladiii <abdulhilal242@gmail.com>
2024-04-13 07:47:16 +0700

    feat : add register form with register logic

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -1,11 +1,23 @@
+"use client";
+
 import Button from "@/components/ui/button";
 import Input from "@/components/ui/input";
 import { zodResolver } from "@hookform/resolvers/zod";
-import Link from "next/link";
 import { useForm } from "react-hook-form";
 import { UserType, userSchema } from "@/types/user";
+import { registerMutation } from "@/queries/userQuery";
+import { useState } from "react";
+import clsx from "clsx";
+import Link from "next/link";
+
+interface Message {
+  message?: string;
+  status?: number;
+}
 
 const RegisterForm = () => {
+  const registerUser = registerMutation();
+  const [message, setMessage] = useState<Message>({});
   const {
     register,
     handleSubmit,
@@ -13,8 +25,13 @@ const RegisterForm = () => {
   } = useForm<UserType>({
     resolver: zodResolver(userSchema),
   });
-  const onSubmit = (data: UserType) => {
-    console.log(data);
+  const onSubmit = async (data: UserType) => {
+    const res = await registerUser.mutateAsync(data);
+    if (typeof res === "string") {
+      setMessage({ message: res });
+    } else {
+      setMessage(res);
+    }
   };
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="w-full space-y-5">
@@ -54,10 +71,27 @@ const RegisterForm = () => {
         placeholder="Password"
         variant="secondary"
       />
-      <div className="flex justify-between items-center">
-        <Button type="submit" className="w-full">
-          Create Account
+      <p
+        className={clsx("text-center text-sm text-black mt-2", {
+          "text-red-500": message.status === 400,
+        })}
+      >
+        {message.message}
+      </p>
+      <div className="flex flex-col justify-between items-center">
+        <Button
+          type="submit"
+          className="w-full"
+          disabled={registerUser.isPending}
+        >
+          {registerUser.isPending ? "Creating..." : "Create Account"}
         </Button>
+        <p className="mt-2">
+          Already have an account?{" "}
+          <Link href="/login" className="underline">
+            Login
+          </Link>
+        </p>
       </div>
     </form>
   );
