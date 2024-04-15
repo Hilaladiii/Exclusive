84fcadb622fe3a57bac746ca287db04becfba52e
Hilaladiii <abdulhilal242@gmail.com>
2024-04-15 14:36:38 +0700

    feat : add constant categories

---
+++ src/common/constant/categories.ts
@@ -0,0 +1,165 @@
+export const CATEGORIES = [
+  {
+    category: "Woman's Fashion",
+    href: "woman-fashion",
+    sub_category: [
+      {
+        sub: "dress",
+      },
+      {
+        sub: "skirt",
+      },
+      {
+        sub: "blouse",
+      },
+      {
+        sub: "pants",
+      },
+      {
+        sub: "jacket",
+      },
+      {
+        sub: "coat",
+      },
+      {
+        sub: "sweater",
+      },
+      {
+        sub: "top",
+      },
+      {
+        sub: "jeans",
+      },
+      {
+        sub: "short",
+      },
+      {
+        sub: "scarf",
+      },
+      {
+        sub: "sunglasses",
+      },
+      {
+        sub: "hat",
+      },
+      {
+        sub: "heels",
+      },
+      {
+        sub: "flats",
+      },
+      {
+        sub: "boots",
+      },
+      {
+        sub: "sandals",
+      },
+      {
+        sub: "handbag",
+      },
+      {
+        sub: "clutch",
+      },
+      {
+        sub: "necklace",
+      },
+    ],
+  },
+  {
+    category: "Men's Fashion",
+    href: "mens-fashion",
+    sub_category: [
+      {
+        sub: "shirt",
+      },
+      {
+        sub: "t-shirt",
+      },
+      {
+        sub: "polo",
+      },
+      {
+        sub: "sweater",
+      },
+      {
+        sub: "hoodie",
+      },
+      {
+        sub: "jacket",
+      },
+      {
+        sub: "coat",
+      },
+      {
+        sub: "blazer",
+      },
+      {
+        sub: "pants",
+      },
+      {
+        sub: "jeans",
+      },
+      {
+        sub: "shorts",
+      },
+      {
+        sub: "sweatpants",
+      },
+      {
+        sub: "suits",
+      },
+      {
+        sub: "tie",
+      },
+      {
+        sub: "bowtie",
+      },
+      {
+        sub: "belt",
+      },
+      {
+        sub: "socks",
+      },
+      {
+        sub: "shoes",
+      },
+      {
+        sub: "sneakers",
+      },
+      {
+        sub: "loafers",
+      },
+      {
+        sub: "boots",
+      },
+    ],
+  },
+  {
+    category: "Electronics",
+    href: "electronics",
+  },
+  {
+    category: "Home & Lifestyle",
+    href: "home-lifestyle",
+  },
+  {
+    category: "Medicine",
+    href: "medicine",
+  },
+  {
+    category: "Sport & Outdoor",
+    href: "sport-outdoor",
+  },
+  {
+    category: "Baby & Toys",
+    href: "baby-toys",
+  },
+  {
+    category: "Groceries & Pet",
+    href: "groceries-pet",
+  },
+  {
+    category: "Health & Beauty",
+    href: "health-beauty",
+  },
+];
