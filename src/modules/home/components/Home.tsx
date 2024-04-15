ead8a24c67e8e6a449b963f9eced01a4e38a51d8
Hilaladiii <abdulhilal242@gmail.com>
2024-04-15 14:37:05 +0700

    feat : add module home page

---
+++ src/modules/home/components/Home.tsx
@@ -0,0 +1,14 @@
+import CategoriesLinkHome from "./BannerSideLink/CategoriesLinkHome";
+import SwiperPromotionHome from "./BannerSideLink/SwiperPromotionHome";
+
+export default function HomePage() {
+  return (
+    <div className="flex w-full flex-col">
+      <div className="flex flex-row justify-between">
+        <CategoriesLinkHome />
+        <div className="mr-20 min-h-full w-1 bg-black/20" />
+        <SwiperPromotionHome />
+      </div>
+    </div>
+  );
+}
