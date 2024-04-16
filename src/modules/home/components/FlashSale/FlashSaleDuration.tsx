cb79693fa5c7579b6d3091b0df5f8d5a3f5ced36
Hilaladiii <abdulhilal242@gmail.com>
2024-04-16 20:40:51 +0700

    refactor : change name file

--- src/modules/home/components/FlashSale/FlashSaleDuration.tsx
+++ src/modules/home/components/FlashSale/FlashSaleDuration.tsx
@@ -5,7 +5,7 @@ const TimerClient = dynamic(
   () => import("@/common/components/elements/Timer"),
   { ssr: false },
 );
-export default function Duration() {
+export default function FlashSaleDuration() {
   return (
     <div className="mt-10 flex flex-row items-end gap-10">
       <h2 className="text-4xl font-semibold">Flash Sales</h2>
