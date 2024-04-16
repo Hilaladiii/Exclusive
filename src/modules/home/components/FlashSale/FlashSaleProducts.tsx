dfd22b95e83ae6fa0a061a5293a28ac6799b6db3
Hilaladiii <abdulhilal242@gmail.com>
2024-04-16 20:41:54 +0700

    feat : add flash sale product section

---
+++ src/modules/home/components/FlashSale/FlashSaleProducts.tsx
@@ -0,0 +1,26 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import CardProduct from "@/common/components/fragments/CardProduct";
+
+export default function FlashSaleProducts() {
+  return (
+    <div className="mt-10">
+      <Swiper
+        slidesPerView={4}
+        spaceBetween={40}
+        pagination={{
+          clickable: true,
+        }}
+        enabled={true}
+      >
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((_, i) => {
+          return (
+            <SwiperSlide key={i}>
+              <CardProduct />
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </div>
+  );
+}
