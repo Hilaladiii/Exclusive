6aec89736b0e99419351337ed274a70582b4fbfe
Hilaladiii <abdulhilal242@gmail.com>
2024-04-17 08:07:08 +0700

    refactor : implement badge section component

--- src/modules/home/components/FlashSale/FlashSaleDuration.tsx
+++ src/modules/home/components/FlashSale/FlashSaleDuration.tsx
@@ -1,4 +1,5 @@
 "use client";
+import BadgeSection from "@/common/components/fragments/BadgeSection";
 import dynamic from "next/dynamic";
 
 const TimerClient = dynamic(
@@ -8,7 +9,7 @@ const TimerClient = dynamic(
 export default function FlashSaleDuration() {
   return (
     <div className="mt-10 flex flex-row items-end gap-10">
-      <h2 className="text-4xl font-semibold">Flash Sales</h2>
+      <BadgeSection title="Today's" section="Flash Sales" />
       <TimerClient targetDateTime="2024-04-30T00:00:00" />
     </div>
   );
