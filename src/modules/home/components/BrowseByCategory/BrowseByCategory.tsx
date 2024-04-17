9f652893f24da90ecc12336a06c86d40bf456a38
Hilaladiii <abdulhilal242@gmail.com>
2024-04-17 08:08:21 +0700

    feat : add browse by category component

---
+++ src/modules/home/components/BrowseByCategory/BrowseByCategory.tsx
@@ -0,0 +1,41 @@
+import BadgeSection from "@/common/components/fragments/BadgeSection";
+import CategoryPhone from "@/common/components/fragments/CategoryPhone";
+import {
+  IconCamera,
+  IconComputer,
+  IconGaming,
+  IconHeadPhone,
+  IconPhone,
+  IconSmartWatch,
+} from "./IconCategory";
+
+export default function BrowseByCategory() {
+  return (
+    <div>
+      <BadgeSection
+        title="Categories"
+        section="Browse By Category"
+      ></BadgeSection>
+      <div className="mt-10 flex flex-row gap-10">
+        <CategoryPhone title="Phones">
+          <IconPhone></IconPhone>
+        </CategoryPhone>
+        <CategoryPhone title="Computers">
+          <IconComputer></IconComputer>
+        </CategoryPhone>
+        <CategoryPhone title="SmartWatch">
+          <IconSmartWatch></IconSmartWatch>
+        </CategoryPhone>
+        <CategoryPhone title="Camera">
+          <IconCamera />
+        </CategoryPhone>
+        <CategoryPhone title="HeadPhones">
+          <IconHeadPhone></IconHeadPhone>
+        </CategoryPhone>
+        <CategoryPhone title="Gaming">
+          <IconGaming />
+        </CategoryPhone>
+      </div>
+    </div>
+  );
+}
