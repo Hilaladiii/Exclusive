55f888b09a84f715f834ede5104663de7b3f28af
Hilaladiii <abdulhilal242@gmail.com>
2024-04-20 21:14:47 +0700

    refactor : change section banner side link

--- src/modules/home/components/Home.tsx
+++ src/modules/home/components/Home.tsx
@@ -1,18 +1,15 @@
-import CategoriesLinkHome from "./BannerSideLink/CategoriesLinkHome";
-import SwiperPromotionHome from "./BannerSideLink/SwiperPromotionHome";
+import BannerSideLink from "./BannerSideLink/BannerSideLink";
+import BestSellingProducts from "./BestSellingProducts/BestSellingProducts";
 import BrowseByCategory from "./BrowseByCategory/BrowseByCategory";
 import FlashSale from "./FlashSale/FlashSale";
 
 export default function HomePage() {
   return (
     <div className="flex w-full flex-col">
-      <div className="flex flex-row justify-between">
-        <CategoriesLinkHome />
-        <div className="mr-20 min-h-full w-1 bg-black/20" />
-        <SwiperPromotionHome />
-      </div>
+      <BannerSideLink />
       <FlashSale />
       <BrowseByCategory />
+      <BestSellingProducts />
     </div>
   );
 }
