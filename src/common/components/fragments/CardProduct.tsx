df42fe9f885613bb781ff3c8e4b495c747ff0800
Hilaladiii <abdulhilal242@gmail.com>
2024-04-28 16:54:33 +0700

    feat : add blur image onload

--- src/common/components/fragments/CardProduct.tsx
+++ src/common/components/fragments/CardProduct.tsx
@@ -3,19 +3,25 @@ import { HiOutlineHeart, HiHeart } from "react-icons/hi";
 import Button from "../elements/Button";
 import Stars from "../elements/Stars";
 import { useState } from "react";
+import { cn } from "@/common/lib/utils";
 
 export default function CardProduct() {
   const [isWishlist, setIsWishlist] = useState<boolean>(false);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
   return (
-    <div className="w-72 rounded shadow-md">
+    <div className="w-72 animate-slide-up rounded shadow-xl ">
       <div className="group relative flex items-center justify-center pb-8 pt-16">
         <Image
           src={"/product.jpg"}
           width={200}
           height={200}
           alt={"product"}
-          className="h-36 w-36 object-contain"
+          className={cn(
+            "h-36 w-36 object-contain",
+            isLoading ? "scale-100 blur-sm" : "scale-100 blur-0",
+          )}
           loading="lazy"
+          onLoad={() => setIsLoading(false)}
         />
         <div className="absolute left-5 top-5">
           <p className="rounded bg-secondary2 px-5 py-2 text-xs text-white">
@@ -35,6 +41,13 @@ export default function CardProduct() {
             onClick={() => setIsWishlist(true)}
           />
         )}
+        <Button
+          variant="black"
+          size="small"
+          className="absolute bottom-0 hidden w-full rounded-t-none text-center group-hover:grid"
+        >
+          Add To Cart
+        </Button>
       </div>
       <div className="px-5 py-4">
         <h3 className="text-base font-medium">HAVIT HV-G92 Gamepad</h3>
@@ -43,9 +56,6 @@ export default function CardProduct() {
           <p className="text-gray-400 line-through">$100</p>
         </div>
         <Stars rating={4.8} />
-        <Button variant="black" size="small" className="mt-4 w-full">
-          Add To Cart
-        </Button>
       </div>
     </div>
   );
