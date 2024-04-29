667c01ad6f616bfa0126b15528ef3ff240441521
Hilaladiii <abdulhilal242@gmail.com>
2024-04-29 09:50:17 +0700

    feat : add middleware authentication user

---
+++ src/middlewares/withAuth.ts
@@ -0,0 +1,30 @@
+import {
+  NextFetchEvent,
+  NextMiddleware,
+  NextResponse,
+  NextRequest,
+} from "next/server";
+
+import { getToken } from "next-auth/jwt";
+
+export default function withAuth(
+  middleware: NextMiddleware,
+  requireAuth: string[] = [],
+) {
+  return async (req: NextRequest, next: NextFetchEvent) => {
+    const pathname = req.nextUrl.pathname;
+
+    if (requireAuth.includes(pathname)) {
+      const token = await getToken({
+        secret: process.env.NEXT_AUTH_SECRET,
+        req,
+      });
+      if (!token) {
+        const url = new URL("/login", req.url);
+        url.searchParams.set("callbacks", encodeURI(req.url));
+        return NextResponse.redirect(url);
+      }
+    }
+    return middleware(req, next);
+  };
+}
