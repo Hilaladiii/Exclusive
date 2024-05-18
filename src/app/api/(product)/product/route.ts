73ad3162c7fed0ca324de130674ad5cc16a8c955
Hilaladiii <abdulhilal242@gmail.com>
2024-05-18 13:51:19 +0700

    feat : add product data api

---
+++ src/app/api/(product)/product/route.ts
@@ -0,0 +1,22 @@
+import { addProduct } from "@/db/dbProduct";
+import { NextRequest } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const productData = await req.formData();
+    const res = await addProduct({
+      name: productData.get("name") as string,
+      description: productData.get("description") as string,
+      price: parseFloat(productData.get("price") as string),
+      rating: parseFloat(productData.get("rating") as string),
+      promotionValue: parseInt(productData.get("promotionValue") as string),
+      image: productData.get("image") as File,
+    });
+    console.log(res);
+    return new Response(res.message, {
+      status: res.status,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
