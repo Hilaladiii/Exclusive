0268cf78bcb5a6dd6811272d466f6b1efd2663da
Hilaladiii <abdulhilal242@gmail.com>
2024-05-19 23:17:56 +0700

    feat : api route get product by id

--- src/app/api/(product)/products/route.ts
+++ src/app/api/(product)/products/route.ts
@@ -1,13 +1,26 @@
-import { getProducts } from "@/db/dbProduct";
-import { NextResponse } from "next/server";
+import { getProductById, getProducts } from "@/db/dbProduct";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
-  const res = await getProducts();
-  return NextResponse.json(
-    {
-      data: res.data,
-      message: res.message,
-    },
-    { status: res.status },
-  );
+export async function GET(req: NextRequest) {
+  const { searchParams } = new URL(req.url);
+  const id = searchParams.get("id");
+  if (id) {
+    const res = await getProductById({ id });
+    return NextResponse.json(
+      {
+        data: res.data,
+        message: res.message,
+      },
+      { status: res.status },
+    );
+  } else {
+    const res = await getProducts();
+    return NextResponse.json(
+      {
+        data: res.data,
+        message: res.message,
+      },
+      { status: res.status },
+    );
+  }
 }
