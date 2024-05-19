9096cc380569310747cd18ea149db8bda22abc6e
Hilaladiii <abdulhilal242@gmail.com>
2024-05-19 23:16:55 +0700

    refactor : title semibold

--- src/common/components/elements/BadgeCategory.tsx
+++ src/common/components/elements/BadgeCategory.tsx
@@ -2,7 +2,7 @@ export default function BadgeCategory({ title }: { title: string }) {
   return (
     <div className="mt-36 flex flex-row items-center gap-4">
       <div className="flex h-10 w-5 rounded bg-secondary2" />
-      <h1 className="text-secondary2">{title}</h1>
+      <h1 className="font-semibold text-secondary2">{title}</h1>
     </div>
   );
 }
