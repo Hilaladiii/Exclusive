d9656e6e7c9f2a9b3cd280d40cd22302e1c499ba
Hilaladiii <abdulhilal242@gmail.com>
2024-05-19 23:20:41 +0700

    feat : add product page

---
+++ src/app/(pages)/products/page.tsx
@@ -0,0 +1,10 @@
+import { MainLayout } from "@/common/components/layouts/MainLayout";
+import Products from "@/modules/products";
+
+export default function ProductPage() {
+  return (
+    <MainLayout>
+      <Products />
+    </MainLayout>
+  );
+}
