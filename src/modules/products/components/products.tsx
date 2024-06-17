1e24163b34bf51b0aea890668f2ee32e68cbae28
Hilaladiii <abdulhilal242@gmail.com>
2024-06-17 17:16:12 +0700

    chore : implement card product skeleton

--- src/modules/products/components/products.tsx
+++ src/modules/products/components/products.tsx
@@ -1,36 +1,41 @@
 "use client";
 
 import CardProduct from "@/common/components/fragments/CardProduct";
+import CardProductSkeleton from "@/common/components/skeletons/CardProductSkeleton";
 import { ProductType } from "@/common/types/product";
 import { getProductsQuery } from "@/queries/productQuery";
 
 export default function Products() {
   const { data, error, isFetching } = getProductsQuery();
+
   if (error) {
-    return <h1>error</h1>;
+    return <h1>Error loading products</h1>;
   }
-  if (isFetching) {
-    return <h1>Loading...</h1>;
-  }
+
   const productData = data?.data.data;
+
   return (
     <div className="w-full">
       <h1 className="my-5 text-3xl font-bold">
-        All Products ({productData.length})
+        All Products ({productData?.length ?? 0})
       </h1>
       <div className="flex flex-wrap gap-5">
-        {productData.length > 0 &&
-          productData.map((product: ProductType, index: number) => (
-            <CardProduct
-              title={product.name}
-              image={product.image}
-              price={product.price}
-              rating={product.rating}
-              promotionValue={product.promotionValue}
-              key={product.id_product}
-              id={product.id_product}
-            />
-          ))}
+        {isFetching
+          ? Array.from({ length: 5 }).map((_, index) => (
+              <CardProductSkeleton key={index} />
+            ))
+          : productData.length > 0 &&
+            productData.map((product: ProductType) => (
+              <CardProduct
+                title={product.name}
+                image={product.image}
+                price={product.price}
+                rating={product.rating}
+                promotionValue={product.promotionValue}
+                key={product.id_product}
+                id={product.id_product}
+              />
+            ))}
       </div>
     </div>
   );
