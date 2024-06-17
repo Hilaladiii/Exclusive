0e6e40bab4e8b9bd7ee90654e118bd04d4d5c2c3
Hilaladiii <abdulhilal242@gmail.com>
2024-06-17 17:17:14 +0700

    feat : query & mutation get wishlist, get wishlist count, unwishlist, wishlist

--- src/queries/productQuery.ts
+++ src/queries/productQuery.ts
@@ -4,6 +4,10 @@ import {
   getProductsById,
   getProductsPromotion,
   getProductsPromotionById,
+  getWishlist,
+  getWishlistCount,
+  unWishlistProduct,
+  wishLisProduct,
 } from "@/services/productService";
 import { UploadProductType } from "@/common/types/product";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
@@ -26,7 +30,7 @@ export function getProductsQuery() {
   return useQuery({
     queryKey: ["products"],
     queryFn: () => getProducts(),
-    staleTime: 1000 * 60 * 5,
+    staleTime: 1000 * 60 * 10,
   });
 }
 
@@ -34,7 +38,7 @@ export function getProductsPromotionQuery() {
   return useQuery({
     queryKey: ["products-promotion"],
     queryFn: () => getProductsPromotion(),
-    staleTime: 1000 * 60 * 5,
+    staleTime: 1000 * 60 * 10,
   });
 }
 
@@ -50,6 +54,67 @@ export function getProductByIdQuery({ id }: { id: string }) {
   return useQuery({
     queryKey: ["products", { id }],
     queryFn: () => getProductsById({ id }),
+    staleTime: Infinity,
+  });
+}
+
+export function wishListProductMutation() {
+  const queryclient = useQueryClient();
+  return useMutation({
+    mutationFn: ({ id, email }: { id: string; email: string }) =>
+      wishLisProduct({ id, email }),
+    onSettled: async (_, error) => {
+      if (error) {
+        throw new Error(error.toString());
+      } else {
+        await queryclient.invalidateQueries({
+          queryKey: ["products"],
+        });
+        await queryclient.invalidateQueries({
+          queryKey: ["products-promotion"],
+        });
+        await queryclient.invalidateQueries({
+          queryKey: ["wishlist"],
+        });
+        await queryclient.invalidateQueries({
+          queryKey: ["wishlist-count"],
+        });
+      }
+    },
+  });
+}
+
+export function getWishlistQuery() {
+  return useQuery({
+    queryKey: ["wishlist"],
+    queryFn: () => getWishlist(),
+    staleTime: Infinity,
+  });
+}
+
+export function unWishlistProductMutation() {
+  const queryclient = useQueryClient();
+  return useMutation({
+    mutationFn: (id_product: string) => unWishlistProduct(id_product),
+    onSettled: async (_, error) => {
+      if (error) {
+        throw new Error(error.toString());
+      } else {
+        await queryclient.invalidateQueries({
+          queryKey: ["wishlist"],
+        });
+        await queryclient.invalidateQueries({
+          queryKey: ["wishlist-count"],
+        });
+      }
+    },
+  });
+}
+
+export function getWishlistCountQuery() {
+  return useQuery({
+    queryKey: ["wishlist-count"],
+    queryFn: () => getWishlistCount(),
     staleTime: Infinity,
   });
 }
