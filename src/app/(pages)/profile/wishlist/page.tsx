6d457bd1356303bcfeb2728f6ec86668263c923c
Hilaladiii <abdulhilal242@gmail.com>
2024-06-17 17:11:33 +0700

    feat : add wishlist page

---
+++ src/app/(pages)/profile/wishlist/page.tsx
@@ -0,0 +1,10 @@
+import { MainLayout } from "@/common/components/layouts/MainLayout";
+import Wishlist from "@/modules/wishlist";
+
+export default function WishlistPage() {
+  return (
+    <MainLayout>
+      <Wishlist />
+    </MainLayout>
+  );
+}
