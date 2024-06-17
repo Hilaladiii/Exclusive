5c79378df0c3012c2236920eea6327cf7cf41c0b
Hilaladiii <abdulhilal242@gmail.com>
2024-06-17 17:12:01 +0700

    feat : get product & get product by id

--- src/app/api/(product)/products/route.ts
+++ src/app/api/(product)/products/route.ts
@@ -1,7 +1,10 @@
 import { getProductById, getProducts } from "@/db/dbProduct";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
+import { authOption } from "../../auth/[[...nextauth]]/option";
 
 export async function GET(req: NextRequest) {
+  const session = await getServerSession(authOption);
   const { searchParams } = new URL(req.url);
   const id = searchParams.get("id");
   if (id) {
@@ -14,13 +17,22 @@ export async function GET(req: NextRequest) {
       { status: res.status },
     );
   } else {
-    const res = await getProducts();
-    return NextResponse.json(
-      {
-        data: res.data,
-        message: res.message,
-      },
-      { status: res.status },
-    );
+    if (session) {
+      const email = session.user!.email;
+      if (!email) {
+        return NextResponse.json(
+          { message: "User email not available in session" },
+          { status: 400 },
+        );
+      }
+      const res = await getProducts(email);
+      return NextResponse.json(
+        {
+          data: res.data,
+          message: res.message,
+        },
+        { status: res.status },
+      );
+    }
   }
 }
