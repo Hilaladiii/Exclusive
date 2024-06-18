396d3b8d4697781adac30155f435eaf2c17c7b87
Hilaladiii <abdulhilal242@gmail.com>
2024-06-18 17:05:23 +0700

    feat : add cart page

---
+++ src/app/(pages)/profile/cart/page.tsx
@@ -0,0 +1,10 @@
+import { MainLayout } from "@/common/components/layouts/MainLayout";
+import Cart from "@/modules/cart";
+
+export default function CartPage() {
+  return (
+    <MainLayout>
+      <Cart />
+    </MainLayout>
+  );
+}
